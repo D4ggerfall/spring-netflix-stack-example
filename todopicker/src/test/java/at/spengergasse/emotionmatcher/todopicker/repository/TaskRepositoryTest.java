d46aa280a135e16d23bd2835d9cf228065a25b54
harpreet <neh16822>
2018-10-29 17:57:15 +0100

    Finished all repository tests for the task.

--- todopicker/src/test/java/at/spengergasse/emotionmatcher/todopicker/repository/TaskRepositoryTest.java
+++ todopicker/src/test/java/at/spengergasse/emotionmatcher/todopicker/repository/TaskRepositoryTest.java
@@ -1,17 +1,22 @@
 package at.spengergasse.emotionmatcher.todopicker.repository;
 
 
-
 import at.spengergasse.emotionmatcher.todopicker.model.Task;
 import org.junit.Test;
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /*
 @SpringBootTest
@@ -23,8 +28,8 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class TaskRepositoryTest {
 
-   @Autowired
-   public TaskRepository taskRepository;
+    @Autowired
+    public TaskRepository taskRepository;
 
 
     @Test
@@ -34,7 +39,35 @@ public class TaskRepositoryTest {
 
         Task task2 = taskRepository.save(task);
 
-        assertEquals(task,task2);
+        assertEquals(task, task2);
+    }
+
+
+    @Test
+    public void deleteTaskTest() {
+
+        Task task = Task.builder().date(LocalDate.now()).description("This is just a test task").importance(1).location("Austria").title("Task Test with a User").build();
+
+        Task task2 = taskRepository.save(task);
+
+        assertEquals(task, task2);
+
+        taskRepository.delete(task);
+
+        Assertions.assertThat(taskRepository.findById(task.getId())).isEmpty();
+
+
+    }
+
+
+    @Test
+    public void findbyIdTest()
+    {
+        Task task = Task.builder().date(LocalDate.now()).description("This is just a test task").importance(1).location("Austria").title("Task Test with a User").build();
+
+        Task task2 = taskRepository.save(task);
+
+        assertNotNull(taskRepository.findById(task.getId()));
     }
 }
 
