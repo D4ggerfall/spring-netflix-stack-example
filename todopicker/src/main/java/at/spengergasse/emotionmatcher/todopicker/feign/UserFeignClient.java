eb8b7cdd52aef05d6d9b3d3572a5d1326afc64f2
Nehar Harpreet <Neh16822@spengergasse.at>
2018-10-29 21:01:25 +0100

    Fixing the feign Client

--- todopicker/src/main/java/at/spengergasse/emotionmatcher/todopicker/feign/UserFeignClient.java
+++ todopicker/src/main/java/at/spengergasse/emotionmatcher/todopicker/feign/UserFeignClient.java
@@ -5,13 +5,14 @@ import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 //import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-//@FeignClient(name="user", configuration = UserFeignConfiguration.class)
+@FeignClient(name="user", configuration = UserFeignConfiguration.class)
 public interface UserFeignClient {
 
 
