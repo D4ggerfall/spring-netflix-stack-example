b336db9ab3ce0456617d76442e5f4e4be0ffb051
Harpreet <neh16822@spengergasse.at>
2018-11-05 11:23:11 +0100

    Fixed dependencies, project now builds correctly

--- spotifysongpicker/src/main/java/at/spengergasse/emotionmatcher/spotifysongpicker/model/Song.java
+++ spotifysongpicker/src/main/java/at/spengergasse/emotionmatcher/spotifysongpicker/model/Song.java
@@ -2,9 +2,7 @@ package at.spengergasse.emotionmatcher.spotifysongpicker.model;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
@@ -17,13 +15,20 @@ import java.io.Serializable;
 public class Song implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "ARTIST")
     private String artist;
+
+    @Column(name = "SONGNAME")
     private String songName;
 
     //could be youtube, spotify, etc.
+    @Column(name = "SONGLINK")
     private String songLink;
+
     //number from 1-3 where 3 is the happiest and 1 is the saddest
+    @Column(name = "EMOTIONLEVEL")
     private int emotionLevel;
 }
--- spotifysongpicker/src/main/resources/data.sql
+++ spotifysongpicker/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Raf Canmora", "Bye, Bye", "https://www.youtube.com/watch?v=xa_eri_abOI", "2");
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Fonola Band", "Bella Ciao", "https://www.youtube.com/watch?v=4CI3lhyNKfo", "3");
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Coldplay", "Fun", "https://www.youtube.com/watch?v=rqCP4Invnrs", "1");
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Toto", "Africa", "https://www.youtube.com/watch?v=FTQbiNvZqaY", "3");
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Ed Sheeran", "I See Fire", "https://www.youtube.com/watch?v=2fngvQS_PmQ", "1");
-INSERT INTO song(artist, songName, songLink, emotionLevel) VALUES ("Alan Walker", "Faded", "https://www.youtube.com/watch?v=60ItHLz5WEA", "1");
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (1,'Raf Camora', 'Bye, Bye', 'https://www.youtube.com/watch?v=xa_eri_abOI', 2);
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (2,'Fonola Band', 'Bella Ciao', 'https://www.youtube.com/watch?v=4CI3lhyNKfo', 3);
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (3,'Coldplay', 'Fun', 'https://www.youtube.com/watch?v=rqCP4Invnrs', 1);
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (4,'Toto', 'Africa', 'https://www.youtsube.com/watch?v=FTQbiNvZqaY', 3);
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (5,'Ed Sheeran', 'I See Fire', 'https://www.youtube.com/watch?v=2fngvQS_PmQ', 1);
+INSERT INTO song(id, artist, songName, songLink, emotionLevel) VALUES (6,'Alan Walker', 'Faded', 'https://www.youtube.com/watch?v=60ItHLz5WEA', 1);
 
 
 
