fb80f91c875d2a84f011a06bae98fbcc231a9886
Harpreet <neh16822@spengergasse.at>
2018-11-05 11:00:31 +0100

    Writting test

---
+++ todopicker/src/test/java/at/spengergasse/emotionmatcher/todopicker/service/TaskServiceTest.java
@@ -0,0 +1,31 @@
+package at.spengergasse.emotionmatcher.todopicker.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskServiceTest {
+
+    @Autowired
+    public TaskService taskService;
+
+    @Test
+    public void createTaskTest()
+    {
+
+    }
+
+
+    @Test
+    public void returnTaskTest()
+    {
+
+    }
+
+
+
+}
