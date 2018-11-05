317f187c40b1965d9374257384d09d70f0578312
Harpreet <neh16822@spengergasse.at>
2018-11-05 11:34:56 +0100

    Writting tests

--- todopicker/src/test/java/at/spengergasse/emotionmatcher/todopicker/service/TaskServiceTest.java
+++ todopicker/src/test/java/at/spengergasse/emotionmatcher/todopicker/service/TaskServiceTest.java
@@ -1,10 +1,16 @@
 package at.spengergasse.emotionmatcher.todopicker.service;
 
+import at.spengergasse.emotionmatcher.todopicker.model.Task;
+import at.spengergasse.emotionmatcher.todopicker.repository.TaskRepository;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -14,16 +20,38 @@ public class TaskServiceTest {
     public TaskService taskService;
 
     @Test
-    public void createTaskTest()
+    public void createTaskAndFINDTest()
     {
+        Task task = Task.builder().date(LocalDate.now()).description("This jut a test task")
+                .importance(1).location("Austria")
+                .title("Task Test with a User")
+                .build();
 
+        Assertions.assertNotNull(task);
+        Assertions.assertNotNull(task.getId());
+
+        Task task2= taskService.createTask(task);
+
+        Assert.assertEquals(task2.getId(),taskService.findbyId(task.getId()));
     }
 
 
     @Test
-    public void returnTaskTest()
+    public void deleteTask()
     {
+        Task task = Task.builder().date(LocalDate.now()).description("This jut a test task")
+                .importance(1).location("Austria")
+                .title("Task Test with a User")
+                .build();
 
+        Assertions.assertNotNull(task);
+        Assertions.assertNotNull(task.getId());
+
+        Task task2= taskService.createTask(task);
+
+        taskService.deletebyId(task.getId());
+
+        org.assertj.core.api.Assertions.assertThat(taskService.findbyId(task.getId())).isEmpty();
     }
 
 
